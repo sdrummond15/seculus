ea2369f5d66ab12cc77610ea5b61b26718fc938b
Silvio <silvio@sdrummond.com.br>
2019-09-23 19:22:22 -0300

    Alterando

--- index.php
+++ index.php
@@ -144,87 +144,12 @@
 
     </div>
 
-</header>
-
-<div id="slide">
-
-    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-
-        <ol class="carousel-indicators">
-
-            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-
-            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-
-            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-
-        </ol>
-
-        <div class="carousel-inner" role="listbox">
-
-            <div class="carousel-item active">
-
-                <div class="banner" style="background-image: url(images/avallis_avaliacao_patrimonial.jpg);">
-
-                    <div>
-
-                        <h1>Avaliação Patrimonial</h1>
-
-                    </div>
-
-                </div>
-
-            </div>
-
-            <div class="carousel-item">
-
-                <div class="banner" style="background-image: url(images/avaliacao_patrimonial_avallis.jpg);">
-
-                    <div>
-
-                        <h1>Avaliação de Bens Móveis e Imóveis</h1>
-
-                    </div>
-
-                </div>
-
-            </div>
-
-            <div class="carousel-item">
-
-                <div class="banner" style="background-image: url(images/avaliacao_patrimonial.jpg);">
-
-                    <div>
-
-                        <h1>Inventário Patrimonial</h1>
-
-                    </div>
-
-                </div>
-
-            </div>
-
-        </div>
-
-        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-
-            <span class="sr-only">Previous</span>
-
-        </a>
-
-        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-
-            <span class="sr-only">Next</span>
-
-        </a>
+    <div id="slide">
 
     </div>
 
-</div>
+</header>
+
 
 <section id="quem-somos">
 
